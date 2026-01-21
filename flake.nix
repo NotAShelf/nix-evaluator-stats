7fe980d27b2e1f9696c9091f9486677b9ccd15d8
NotAShelf <raf@notashelf.dev>
2026-01-21 15:02:50 +0300

    nix: initial tooling
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I5e9bfbc614604329d54240c02f620e156a6a6964

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "NodeJS Project Template";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsForEach = nixpkgs.legacyPackages;
+  in {
+    packages = forEachSystem (system: {
+      default = pkgsForEach.${system}.callPackage ./nix/package.nix {};
+    });
+
+    devShells = forEachSystem (system: {
+      default = pkgsForEach.${system}.callPackage ./shell.nix {};
+    });
+
+    hydraJobs = self.packages;
+  };
+}
---
+++ nix/package.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenv,
+  fetchPnpmDeps,
+  nodejs-slim,
+  pnpm,
+  pnpmConfigHook,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "nix-evaluator-stats";
+  version = "0.1.0";
+
+  src = ../.;
+
+  nativeBuildInputs = [
+    nodejs-slim
+    pnpm
+    pnpmConfigHook # dependency resolution
+  ];
+
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs) pname src;
+    hash = "sha256-CpfaUvQbvOXljCqNwyMUJNDisnlrWCHBwhmdIzjmv+c=";
+    fetcherVersion = 3; # https://nixos.org/manual/nixpkgs/stable/#javascript-pnpm-fetcherVersion
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p $out/share/dist
+    pnpm run build --outDir $out/share/dist
+
+    runHook postBuild
+  '';
+
+  meta = {
+    description = "Pretty visualiser for Nix evaluator stats";
+    homepage = "https://github.com/notashelf/nix-evaluator-stats";
+    platforms = ["x86_64-linux" "aarch64-linux"];
+    license = lib.licenses.mpl20;
+    maintainers = [lib.maintainers.NotAShelf];
+  };
+})
